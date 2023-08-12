6ea400543b7183453d0d736460b39eb82e84c669
Scott Opell <me@scottopell.com>
2023-08-12 14:12:18 -0400

    draws and moves a cursor

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#![warn(clippy::pedantic)]
+mod editor;
+mod terminal;
+
+use editor::Editor;
+pub use editor::Position;
+pub use terminal::Terminal;
+
+fn main() {
+    Editor::default().run();
+}
---
+++ src/terminal.rs
@@ -0,0 +1,77 @@
+use crate::Position;
+use std::io::{self, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal};
+
+pub struct Size {
+    pub width: u16,
+    pub height: u16,
+}
+
+pub struct Terminal {
+    size: Size,
+    _stdout: RawTerminal<std::io::Stdout>,
+}
+
+impl Terminal {
+    /// # Errors
+    /// Bubbles up errors from retrieving terminal size and entering raw mode
+    pub fn new() -> Result<Self, std::io::Error> {
+        let size = termion::terminal_size()?;
+        Ok(Self {
+            size: Size {
+                width: size.0,
+                height: size.1,
+            },
+            _stdout: stdout().into_raw_mode()?,
+        })
+    }
+
+    pub fn clear_screen() {
+        print!("{}", termion::clear::All);
+    }
+
+    #[allow(clippy::cast_possible_truncation)]
+    pub fn cursor_position(pos: &Position) {
+        let Position { mut x, mut y } = pos;
+        x = x.saturating_add(1);
+        y = y.saturating_add(1);
+        let x = x as u16;
+        let y = y as u16;
+        print!("{}", termion::cursor::Goto(x, y));
+    }
+
+    /// # Errors
+    /// Bubbles up errors from underlying flush
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
+    }
+
+    pub fn cursor_hide() {
+        print!("{}", termion::cursor::Hide);
+    }
+
+    pub fn cursor_show() {
+        print!("{}", termion::cursor::Show);
+    }
+
+    /// # Errors
+    /// Bubbles up errors from underlying io
+    pub fn read_key() -> Result<Key, std::io::Error> {
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
+    }
+
+    #[must_use]
+    pub fn size(&self) -> &Size {
+        &self.size
+    }
+}
